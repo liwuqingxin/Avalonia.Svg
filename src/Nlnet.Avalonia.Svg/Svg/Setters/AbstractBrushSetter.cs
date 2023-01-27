88de8ec2d6d4464fe116a819140551895cf45013
NLNet <yangqi1990917@163.com>
2023-01-28 00:16:28 +0800

    ● Paint property (IBrush) supports default value when use url;

--- src/Nlnet.Avalonia.Svg/Svg/Setters/AbstractBrushSetter.cs
+++ src/Nlnet.Avalonia.Svg/Svg/Setters/AbstractBrushSetter.cs
@@ -17,12 +17,18 @@ public abstract class AbstractBrushSetter : AbstractDeferredSetter
 
     public override void InitializeDeferredValue(ISvgContext context, string deferredSetterValue)
     {
-        // url example : "url(#linearGradient-1)"
-        var match = Regex.Match(deferredSetterValue, "url\\(\\#(.*)\\)");
+        // https://www.w3.org/TR/SVG2/painting.html#SpecifyingPaint
+        // url example : "url(#linearGradient-1)" or "url(#linearGradient-1) blue"
+        var match = Regex.Match(deferredSetterValue, "url\\(\\#(.*)\\)\\s*(.*)");
         if (match.Success)
         {
             var id = match.Groups[1].Value;
             context.Brushes.TryGetValue(id, out Value);
+            if (Value == null && match.Groups.Count > 2)
+            {
+                var defaultValueString = match.Groups[2].Value;
+                defaultValueString.TryToIBrush(out Value);
+            }
         }
     }
 }
