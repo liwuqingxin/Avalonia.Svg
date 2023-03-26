0b4784409367dbc9fc99c6808e4e7a6b3e3c70c1
NLNet <yangqi1990917@163.com>
2023-03-27 00:59:56 +0800

    ~

---
+++ src/Nlnet.Avalonia.Svg.Sample/EnumExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Avalonia.Markup.Xaml;
+
+namespace Nlnet.Avalonia.Svg.Sample
+{
+    /// <summary>
+    /// Xaml markup to get the enum values.
+    /// </summary>
+    public class EnumExtension : MarkupExtension
+    {
+        [ConstructorArgument(nameof(Type))]
+        public Type Type { get; set; }
+
+        public EnumExtension(Type type)
+        {
+            Type = type;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Enum.GetValues(Type);
+        }
+    }
+}
