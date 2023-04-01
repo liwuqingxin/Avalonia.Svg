240c1221305818ed7fa2bc3482150cd70d52bb5b
NLNet <yangqi1990917@163.com>
2023-04-02 02:44:37 +0800

    ● Fix up CalculateRadian in different quadrant. ● Calculate radian of Path's StartMarker. (Except for arc segment)

--- src/Nlnet.Avalonia.Svg/Svg/Elements/SvgMarkerable.cs
+++ src/Nlnet.Avalonia.Svg/Svg/Elements/SvgMarkerable.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Reflection.Metadata;
 using Avalonia;
 using Avalonia.Media;
 using Nlnet.Avalonia.Svg.Utils;
@@ -15,26 +16,47 @@ public abstract class SvgMarkerable : SvgShape, ISvgMarkerable
 
     protected static double CalculateRadian(Point p1, Point p2)
     {
+        // in second quadrant
         var angle = Math.Atan((p2.Y - p1.Y) / (p2.X - p1.X));
         if (p2.X < p1.X)
         {
+            // in third quadrant && first quadrant
             angle += Math.PI;
+        }
+        if (p2.X > p1.X & p2.Y < p1.Y)
+        {
+            // in first quadrant
+            angle += Math.PI * 2;
         }
         return angle;
     }
 
     protected static double CalculateRadian(Point p1, Point p2, Point p3)
     {
+        // in second quadrant
         var angle1 = Math.Atan((p2.Y - p1.Y) / (p2.X - p1.X));
         if (p2.X < p1.X)
         {
+            // in third quadrant && first quadrant
             angle1 += Math.PI;
         }
+        if (p2.X > p1.X & p2.Y < p1.Y)
+        {
+            // in first quadrant
+            angle1 += Math.PI * 2;
+        }
 
+        // in second quadrant
         var angle2 = Math.Atan((p3.Y - p2.Y) / (p3.X - p2.X));
         if (p3.X < p2.X)
         {
+            // in third quadrant && first quadrant
             angle2 += Math.PI;
+        }
+        if (p3.X > p2.X & p3.Y < p2.Y)
+        {
+            // in first quadrant
+            angle2 += Math.PI * 2;
         }
 
         return (angle1 + angle2) / 2;
--- src/Nlnet.Avalonia.Svg/Svg/Tags/Visuals/Shapes/SvgPath.cs
+++ src/Nlnet.Avalonia.Svg/Svg/Tags/Visuals/Shapes/SvgPath.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using Avalonia;
 using Avalonia.Media;
 using Nlnet.Avalonia.Svg.CompileGenerator;
 
@@ -31,41 +32,46 @@ public class SvgPath : SvgMarkerable, ISvgShape, ISvgGraphic, ISvgRenderable, IS
 
             if (figure.Segments is { Count: > 0 })
             {
+                var segment = figure.Segments[0];
+                radian = segment switch
+                {
+                    ArcSegment s1 => CalculateRadian(point, s1.Point),
+                    LineSegment s2 => CalculateRadian(point, s2.Point),
+                    PolyLineSegment s3 => CalculateRadian(point, s3.Points[0]),
+                    BezierSegment s4 => CalculateRadian(point, s4.Point1),
+                    QuadraticBezierSegment s5 => CalculateRadian(point, s5.Point1),
+                    _ => radian
+                };
 
+                if (figure.IsClosed)
+                {
+                    var lastSegment = figure.Segments.Last();
+                    var radian2 = lastSegment switch
+                    {
+                        ArcSegment s1 => CalculateRadian(s1.Point, point),
+                        LineSegment s2 => CalculateRadian(s2.Point, point),
+                        PolyLineSegment s3 => CalculateRadian(s3.Points.Last(), point),
+                        BezierSegment s4 => CalculateRadian(s4.Point3, point),
+                        QuadraticBezierSegment s5 => CalculateRadian(s5.Point2, point),
+                        _ => radian
+                    };
+
+                    radian = (radian2 + radian) / 2;
+                }
             }
-            var segment = figure.Segments[0];
-            
+
             RenderMarkerOnPoint(dc, ctx, marker, point, radian, true);
         }
     }
 
     public override void RenderMarkerEnd(DrawingContext dc, ISvgContext ctx, SvgMarker marker)
     {
-        if (OriginalGeometry is PolylineGeometry polyline && polyline.Points.Count > 0)
-        {
-            var point  = polyline.Points.Last();
-            var radian = 0d;
-            if (polyline.Points.Count > 1)
-            {
-                var lastPoint = polyline.Points[^2];
-                radian = CalculateRadian(lastPoint, point);
-            }
-            RenderMarkerOnPoint(dc, ctx, marker, point, radian, false);
-        }
+        这里
+        // TODO
     }
 
     public override void RenderMarkerMid(DrawingContext dc, ISvgContext ctx, SvgMarker marker)
     {
-        if (OriginalGeometry is PolylineGeometry polyline && polyline.Points.Count > 2)
-        {
-            for (var i = 1; i < polyline.Points.Count - 1; i++)
-            {
-                var p1     = polyline.Points[i - 1];
-                var p2     = polyline.Points[i];
-                var p3     = polyline.Points[i + 1];
-                var radian = CalculateRadian(p1, p2, p3);
-                RenderMarkerOnPoint(dc, ctx, marker, p2, radian, false);
-            }
-        }
+        // TODO
     }
 }
